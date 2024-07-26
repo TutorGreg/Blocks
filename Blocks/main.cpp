1d49eaf2cb67e2855a1d3e4c3b3c753a26a28e9a
ctrl-alt-greg <65520831+ctrl-alt-greg@users.noreply.github.com>
2024-07-25 20:46:38 -0400

    Initial Commit

---
+++ Blocks/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Blocks
+//
+//  Created by Gregory Sakas on 7/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
